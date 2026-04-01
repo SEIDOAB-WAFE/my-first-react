e6b82ba04e19162d878d519ec19b3434f55ac06a
Martin <martin_lenart@icloud.com>
2026-04-01 09:34:12 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {Hello} from './Hello';
 import './App.css';
 import {Cards} from './components/Cards';
+
 function App() {
   return (
     <>
-    <Cards/>
+    <Cards title='My pics' imgs={['img1.png', 'img2.png']} headings={['Card Heading 1', 'Card Heading 2']}/>
     </>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,11 +3,13 @@ import '../css/bootstrap/bootstrap.css';
 
 export function Cards(props) {
 
+  const title = props.title || 'Cards';
   const images = props.imgs || ['img1.png', 'img2.png'];
   const headings = props.headings || ['heading1', 'heading2'];
 
   return (
         <div className="container px-4 py-4" id="cards">
+          <h2 className="pb-2 border-bottom">{title}</h2>
           <div className="d-flex flex-wrap justify-content-between bg-body-tertiary p-5 rounded">
 
             {
